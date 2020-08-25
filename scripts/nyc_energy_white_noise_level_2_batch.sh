20a46db66485b67909dea118fc01d85c8e10f178
thalvari <tuomas.halvari@helsinki.fi>
2020-08-25 08:00:26 +0300

    add more nyc energy white noise preds

--- scripts/nyc_energy_white_noise_level_2_batch.sh
+++ scripts/nyc_energy_white_noise_level_2_batch.sh
@@ -4,4 +4,3 @@ python run.py analytics-zoo train/nyc_energy_white_noise_level_1.csv 200 timeSta
 python run.py azure_automl train/nyc_energy_white_noise_level_1.csv 200 timeStamp demand
 python run.py tpot_automl train/nyc_energy_white_noise_level_1.csv 200 timeStamp demand
 python run.py tpot_automl train/nyc_energy_white_noise_level_1.csv 200 timeStamp demand
-
--- scripts/nyc_energy_white_noise_level_3_batch.sh
+++ scripts/nyc_energy_white_noise_level_3_batch.sh
@@ -2,6 +2,5 @@
 
 python run.py analytics-zoo train/nyc_energy_white_noise_level_1.csv 200 timeStamp demand
 python run.py azure_automl train/nyc_energy_white_noise_level_1.csv 200 timeStamp demand
-python run.py tpot_automl train/nyc_energy_white_noise_level_1.csv 200 timeStamp demand
-python run.py tpot_automl train/nyc_energy_white_noise_level_1.csv 200 timeStamp demand
-
+python run.py analytics-zoo train/nyc_energy_white_noise_level_2.csv 200 timeStamp demand
+python run.py analytics-zoo train/nyc_energy_white_noise_level_5.csv 200 timeStamp demand
