66153c05bc49499e7cb7ef90c99f450ebbc14d1c
thalvari <tuomas.halvari@helsinki.fi>
2020-09-07 15:04:01 +0300

    small updates

--- scripts/test_batch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python run_test.py tpot_automl train/nyc_taxi_stuck-at_level_0.csv 2064 timestamp value 600
-python run_test.py tpot_automl train/nyc_taxi_stuck-at_level_0.csv 2064 timestamp value 600
-python run_test.py tpot_automl train/nyc_taxi_stuck-at_level_1.csv 2064 timestamp value 600
-python run_test.py tpot_automl train/nyc_taxi_stuck-at_level_1.csv 2064 timestamp value 600
