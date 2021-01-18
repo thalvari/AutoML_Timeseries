385c77dbfdfdd58727f7ef4bfe229d7f1ef5bd3a
thalvari <tuomas.halvari@helsinki.fi>
2021-01-18 22:32:41 +0200

    more results

---
+++ scripts/temperature_multi_stuck-at_prophet_iterative_6h_batch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python run.py prophet_iterative train/temperature_multi_stuck-at_level_0.csv 2223 datetime Montreal 360
+python run.py prophet_iterative train/temperature_multi_stuck-at_level_1.csv 2223 datetime Montreal 360
+python run.py prophet_iterative train/temperature_multi_stuck-at_level_2.csv 2223 datetime Montreal 360
+python run.py prophet_iterative train/temperature_multi_stuck-at_level_3.csv 2223 datetime Montreal 360
+python run.py prophet_iterative train/temperature_multi_stuck-at_level_4.csv 2223 datetime Montreal 360
+python run.py prophet_iterative train/temperature_multi_stuck-at_level_5.csv 2223 datetime Montreal 360
---
+++ scripts/temperature_white_noise_prophet_6h_batch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python run.py prophet train/temperature_white_noise_level_0.csv 2223 datetime Montreal 360
+python run.py prophet train/temperature_white_noise_level_1.csv 2223 datetime Montreal 360
+python run.py prophet train/temperature_white_noise_level_2.csv 2223 datetime Montreal 360
+python run.py prophet train/temperature_white_noise_level_3.csv 2223 datetime Montreal 360
+python run.py prophet train/temperature_white_noise_level_4.csv 2223 datetime Montreal 360
+python run.py prophet train/temperature_white_noise_level_5.csv 2223 datetime Montreal 360
